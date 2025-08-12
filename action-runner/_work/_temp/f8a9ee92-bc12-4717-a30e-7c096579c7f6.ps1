df7dc7438270d2e068e45ef131440c83a8985a93
unknown <jauharimalikupil@gmail.com>
2025-08-12 19:58:40 +0700

    clean

--- action-runner/_work/_temp/f8a9ee92-bc12-4717-a30e-7c096579c7f6.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = 'stop'
-echo "C:\Users\PROGRAM-002\AppData\Roaming\npm" | Out-File -FilePath $env:GITHUB_PATH -Append -Encoding utf8
-if ((Test-Path -LiteralPath variable:\LASTEXITCODE)) { exit $LASTEXITCODE }
